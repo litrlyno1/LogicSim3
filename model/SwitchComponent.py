65451765d6054cdb8fea9b4ca2a3d69addbcfe93
litrlyno1 <alex.glushchenko@outlook.com>
2025-09-28 13:10:39 +0200

    minor fixed

--- model/SwitchComponent.py
+++ model/SwitchComponent.py
@@ -1,10 +1,11 @@
 from Interfaces import IToggleable, ISignalSource
 from Observer import SignalPropagator
+from LogicGate import LogicGate
 
-class Switch(IToggleable, ISignalSource):
+class Switch(LogicGate, IToggleable, ISignalSource):
     
     def __init__(self, propagator : SignalPropagator = None):
-        super().__init__(propagator)
+        super().__init__(numInputs=0, numOutputs=1, propagator = propagator)
         self._value = False
     
     def toggle(self):
